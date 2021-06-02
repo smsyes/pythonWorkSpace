db00180061e15e911561b0006d5870cb1efea816
sms <39462582+smsyes@users.noreply.github.com>
2021-06-02 11:40:46 +0900

    Add files via upload

---
+++ main/mApplication/modules/python.mod
@@ -0,0 +1,2 @@
++ Rigging_Tool 1.0 C:\Users\minsung\Documents\maya\2019\scripts
+scripts: C:\Users\minsung\Documents\maya\2019\scripts
---
+++ main/mApplication/modules/scripts/userSetup.py
@@ -0,0 +1,4 @@
+import pymel.core as pm
+pm.evalDeferred('print "import ms tool"')
+pm.evalDeferred('import markingMenu;markingMenu.markingMenu()')
+
