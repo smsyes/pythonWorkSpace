e5bd084e5a036194f167a400a19ada8721694dbc
sms <maruzou89@gmail.com>
2021-06-29 19:31:02 +0900

    20210629

--- main/mApplication/ms_module/lib/logic_node_editor/node_editor/lib/rebuild.py
+++ main/mApplication/ms_module/lib/logic_node_editor/node_editor/lib/rebuild.py
@@ -17,67 +17,100 @@ blah blah blah blah blah blah
 #
 from pymel.core import *
 
-def connect_attr(*args):
-    connectAttr('{}.{}'.format(args[0], args[1]),
-                '{}.{}'.format(args[2], args[3]))
+def checkAttr(object_,attr):
+    """Return Check the existence of attribute in object
 
+    Arguments:
+        object_ (node): An object with attribute attributes
+        attr (attributes): attribute long name
 
-def connect_attrs(object_, output, input):
-    items, targets = divide_in_two(object_)
-    for i, item in enumerate(items):
-        connect_attr(item, output, targets[i], input)
-        
+    Returns:
+        bool
+    """
+    attrExist = attributeQuery(attr, node=object_, exists=True)
+    print "attrExist is {}".format(attrExist)
+    return attrExist
 
-def checkAttrExist(obj,attr,type,replace):
-    attrExist = attributeQuery(attr, node=obj, exists=True)
-    newAttr = ''
-    if(attrExist == False):
-        newAttr = addAttr(obj, longName=attr, at=type)
-         
-    else:
-        if(replace == True):
-            deleteAttr(obj, at=attr)
-            newAttr = addAttr(obj, longName=attr, at=type)
-             
-    newAttr = PyNode('{}.{}'.format(obj, attr))
-    return (attrExist,newAttr)
+def checkConnection(attrs):
+    """Return Check if properties are connected
+
+    Arguments:
+        attrs (node): PyNode(attributes)
+
+    Returns:
+        bool
+    """
+    connection = isConnected(attrs[0],attrs[1])
+    print "connection is {}".format(connection)
+    return connection
+
+def divide_in_two(object_):
+    """Return list split in half
+
+    Arguments:
+        object_ (node): list
+
+    Returns:
+        each half of the list
+    """
+    divideNum = len(object_)/2
+    items = object_[:divideNum]
+    targets = object_[divideNum:]
+    return items, targets
+
+def objectAttr(object_, attr):
+    """Return attributes
+
+    Arguments:
+        object_ (node): node with attributes
+        attr (attribute): attribute long name
+    Returns:
+        PyNode(attributes)
+    """
+    attr_ = PyNode('{}.{}'.format(object_, attr))
+    print attr_
+    return attr_
     
 
-def msg_check(object_, msg):
-    objects_ = []
-    msgAttr_ = attributeQuery(msg, node=object_, msg=1)
+def msg_check(object_, msg_):
+    list_ = []
+    msgAttr_ = attributeQuery(msg_, node=object_, msg=1)
     if msgAttr_ == True:
         pAttrs_ = object_.listConnections(p=1)
         for pAttr in pAttrs_:
             pln = str(pAttr.longName())
-            if pln == msg:
-                objects_.append(pAttr.node())
-    return objects_
+            if pln == msg_:
+                list_.append(pAttr.node())
+    return list_
 
-def connect_(attrDict, objects_):
-    for object in objects_:
-        for i,attr in enumerate(cntAttrs.values()):
-            if object.hasAttr(attr):
-                itemAttr = '{}.{}'.format(object, attr)
-                targetAttr = '{}.{}'.format(object,cntAttrs.keys()[i])
-                connectAttr(itemAttr, targetAttr)
-                print "Connect {0} to {1}".format(itemAttr, targetAttr)
 
-def dis_connect(attrDict, objects_):
-    for object in objects_:
-        for i,attr in enumerate(cntAttrs.values()):
-            if object.hasAttr(attr):
-                itemAttr = '{}.{}'.format(object, attr)
-                targetAttr = '{}.{}'.format(object,cntAttrs.keys()[i])
-                object.disconnectAttr(attr, targetAttr)
-                print "Disconnect {0} to {1}".format(itemAttr, targetAttr)
+object_ = PyNode("arm_GRP")
+fit_ = PyNode("L:fit_arm")
+
+msg_ = {0:"reBuildMode", 1:"input", 2:"init"}
+
+selfAttr_ = {'t':"rebuildTrans", 'r':"rebuildRot"}
+fitAttr_ = {'t':'t', 'r':'r'}
+
+self_ = msg_check(object_, msg_[0])
+input_ = msg_check(object_, msg_[1])
+init_ = msg_check(fit_, msg_[2])
+
+selfList = {0:self_, 1:self_}
+fitList = {0:init_, 1:input_}
+
+def keyAttrDict(list_, attrs):
+    list_ = {0:[], 1:[]}
+    for list in list_:
+        for obj in list:
+            for i,attr in enumerate(attrs.keys()):
+                if checkAttr(obj,attr):
+                    attr_ = objectAttr(obj, attr)
+                    list_[i].append(attr_)
+
+
+keyAttrDict(selfList, selfAttr_)
 
     
-object_ = PyNode("arm_GRP")
-msg = "reBuildMode"
-objects_ = msg_check(object_, msg)
-cntAttrs = {'t':"rebuildTrans", 'r':"rebuildRot"}
-connect_(cntAttrs, objects_)
-# dis_connect(cntAttrs, objects_)
 
 
