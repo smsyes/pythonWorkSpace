b10cea44f334403e537491012164f09d850d615c
sms <maruzou89@gmail.com>
2021-07-05 01:26:07 +0900

    20210705

--- main/mApplication/ms_module/modules/python.mod
+++
@@ -1,2 +0,0 @@
-+ Rigging_Tool 1.0 C:\Users\minsung\Documents\maya\2019\scripts
-scripts: C:\Users\minsung\Documents\maya\2019\scripts
--- main/mApplication/ms_module/modules/scripts/userSetup.py
+++
@@ -1,4 +0,0 @@
-import pymel.core as pm
-pm.evalDeferred('print "import ms tool"')
-pm.evalDeferred('import markingMenu;markingMenu.markingMenu()')
-
