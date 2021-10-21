a7cc19466bd3f98cedea2022369ca3fae9eada3f
sms <maruzou89@gmail.com>
2021-10-21 19:20:50 +0900

    20211021

---
+++ main/mApplication/ms_module/lib/util/jlr_copy_deformer_weights-master/README.md
@@ -0,0 +1,17 @@
+
+### jlr_copy_deformer_weights.py - Python Script
+**Author: Juan Carlos Lara.**
+
+**Description:**
+
+Tool for copy the deformer weights from one deformer to other deformer.
+
+**Install:**
+
+1- Copy the script file "jlr_copy_deformer_weights.py" into your scripts directory.
+
+2- In the script editor add the following lines:
+
+    import jlr_copy_deformer_weights as cdw
+    
+    cdw.open_copy_deformer_weights()
--- main/mApplication/ms_module/rigSupport/lib/_jointLabel.py
+++ main/mApplication/ms_module/rigSupport/lib/_jointLabel.py
@@ -2,43 +2,98 @@ from pymel.core import *
 from rigSupport.lib import config
 
 
-def hierarchy_(object_, type_=None):
+def getChildren_(object_, type_=None):
     object_ = PyNode(object_)
     if not type_:
         type_ = 'transform'
-    hier_ = object_.listRelatives(ad=1, c=1, typ=type_)
-    hier_ = hier_ + [object_]
-    hier_.reverse()
-    return hier_
+    child_ = object_.listRelatives(ad=1, c=1, typ=type_)
+    child_ = child_ + [object_]
+    child_.reverse()
+    return child_
 
+def joint_orient(object_, **kwargs):
+    for joint_ in object_:
+        joint(object_, **kwargs)
+        if joint_ == object_[-1]:
+            joint_.attr('jo').set(0,0,0)
+
+def freezeTransform_(object_):
+    makeIdentity(object_,apply=1,t=1,r=1,s=1,n=0,pn=1)
 
 def loadConfig_(jsonName):
     return config.jsonImport(jsonName)
 
-def jointlabeling_(part, side):
-    config_ = loadConfig_("configDict.json")
-    
-    sel = ls(sl=1)
-    
-    joints = hierarchy_(sel[0], type_='joint')
-    parts_ = config_["jointLabel"][part]
-    side_ = config_["sideName"]
-    sideName_ = side_[side]
-    type_ = config_["extensionsName"]
-    
+
+def jointlabeling_(object_, parts_=None, sideName_=None, sideIndex=None, type_=None, oj_=None, sao_=None):
+    joints = getChildren_(object_, type_='joint')
     for i,jnt in enumerate(joints):
-        if side == 0:
+        if sideIndex == 0:
             name_ = parts_[i] 
         else:
             name_ = '{0}{1}'.format(sideName_,parts_[i])
         rename(jnt, '{0}{1}'.format(name_, type_[1]))
-        jnt.attr('otherType').set(name_)
-        if side > 3:
+        jnt.attr('otherType').set(parts_[i])
+        if sideIndex > 3:
             jnt.attr('side').set(3)
         else:
-            jnt.attr('side').set(side)
+            jnt.attr('side').set(sideIndex)
         jnt.attr('type').set(18)
     
+    freezeTransform_(joints)
+    joint_orient(joints,
+                 e=True,
+                 oj=oj_,
+                 sao=sao_,
+                 zso=True
+                 )
     return joints
+
+def reverseAxis(object_, axis='xzy'):
+    if axis == 'xyz':
+        set_ = 'rz'
+    elif axis == 'yxz':
+        set_ = 'rz'
+    elif axis == 'yzx':
+        set_ = 'rx'
+    elif axis == 'zxy':
+        set_ = 'ry'
+    elif axis == 'zyx':
+        set_ = 'rx'
+    else:
+        set_ = 'ry'
+    for i in object_:
+        attr_ = i.attr(set_)
+        attr_.set(180)
         
-joints_ = jointlabeling_("spine", 0)
+def hierarchy_(object_):
+    for i,obj in enumerate(object_):
+        if i>0:
+            parent(obj, object_[i-1])
+
+def mirror(object_, oj_=None):
+    joints = getChildren_(object_, type_='joint')
+    freezeTransform_(joints)
+    [parent(jnt, w=1) for jnt in joints if jnt.getParent()]
+    reverseAxis(joints, axis=oj_)
+    freezeTransform_(joints)
+    hierarchy_(joints)
+
+
+type="millionvolt"
+part="arm"
+sideIndex=1
+label_ = loadConfig_("configDict.json")
+jo_ = loadConfig_("jointOrientDict.json")
+parts_ = label_["jointLabel"][part]
+side_ = label_["sideName"]
+sideName_ = side_[sideIndex]
+type_ = label_["extensionsName"]
+oj_ = jo_[type][part]["oj"]
+sao_ = jo_[type][part]["sao"]
+
+sel = ls(sl=1)
+# joints_ = jointlabeling_(sel[0], parts_=None, side_=None, sideIndex=None, type_=None, oj=None, sao=None)
+# mirror(sel[0], oj_=oj_)
+
+
+
