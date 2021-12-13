0412dfb5be3a6ca56effacd51396593a2ec24215
sms <maruzou89@gmail.com>
2021-12-14 02:07:35 +0900

    20211214

--- main/mApplication/ms_module/maya/python3/rigSupport/lib/eyeRigIng.py
+++ main/mApplication/ms_module/maya/python3/rigSupport/lib/eyeRigIng.py
@@ -27,19 +27,51 @@ def CurveAtObjectPosition(object_):
     pos_ = [i.getMatrix(worldSpace=True)[-1][:-1] for i in object_]
     curve(n='{0}Crv'.format(name_),d=1,p=pos_)
 
+def surfaceAtPos(object_):
+    surfShape_ = object_[-1].getShape()
+    
+    for i in object_[:-1]:
+        posShape_ = i.getShape()
+        name_ = i.name()
+        cs_ = createNode('closestPointOnSurface', n='{0}CS'.format(name_))
+        ps_ = createNode('pointOnSurfaceInfo', n='{0}PS'.format(name_))
+        rh_ = createNode('rotateHelper', n='{0}RH'.format(name_))
+        SurfPos_ = createNode('transform', n='{0}SurfPos'.format(name_))
+        posShape_.worldPosition >> cs_.inPosition
+        surfShape_.ws >> cs_.inputSurface
+        surfShape_.ws >> ps_.inputSurface
+        cs_.parameterU >> ps_.parameterU
+        cs_.parameterV >> ps_.parameterV
+        ps_.normalizedNormal >> rh_.forward
+        ps_.normalizedTangentU >> rh_.up
+        ps_.position >> SurfPos_.t
+        rh_.r >> SurfPos_.r
 
+def JntAtCurveParam(numList, curve_):
+    shape_ = curve_.getShape()
+    for i,num in enumerate(numList):
+        name_ = '{0}{1}'.format(curve_.name(),i)
+        pc_ = createNode('pointOnCurveInfo', n='{0}PC'.format(name_))
+        jnt_ = joint(n='{0}Jnt'.format(name_))
+        shape_.ws >> pc_.inputCurve
+        pc_.attr('parameter').set(num)
+        pc_.position >> jnt_.t
 
-sel = ls(sl=1,fl=1,r=1)
+sel = ls(sl=1)
 
-curve_ = sel[0]
+# curve_ = sel[0]
 # number = 8
 # numList = division(number)
 # numList = [0,1,2,3,4,5,6,7]
 # LocAtCurveParam(numList, curve_)
 # CurveAtObjectPosition(sel)
-
+# surfaceAtPos(sel)
+shape_ = sel[0].getShape()
+numList = range(shape_.numEPs())
+JntAtCurveParam(numList, sel[0])
 '''
-ef_ = polyListComponentConversion(sel, tf=True,ff=True)
-for i in sel:
-    polyListComponentConversion(i, )
+upLidCrv = PyNode(polyToCurve(n='upLid',form=0,degree=1,usm=1)[0])
+shape_ = upLidCrv.getShape()
+numList = range(shape_.numCVs())
+LocAtCurveParam(numList, upLidCrv)
 '''
