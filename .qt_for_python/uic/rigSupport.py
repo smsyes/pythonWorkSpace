bc8a9b3f2507ad24890e868732a8361f4d52f6e6
sms <maruzou89@gmail.com>
2021-12-15 03:27:38 +0900

    20211215

---
+++ .qt_for_python/uic/rigSupport.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'rigSupport.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(359, 584)
+        self.verticalLayout = QVBoxLayout(Form)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.label_12 = QLabel(Form)
+        self.label_12.setObjectName(u"label_12")
+        font = QFont()
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_12.setFont(font)
+
+        self.verticalLayout.addWidget(self.label_12)
+
+        self.line_5 = QFrame(Form)
+        self.line_5.setObjectName(u"line_5")
+        self.line_5.setFrameShape(QFrame.HLine)
+        self.line_5.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_5)
+
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        font1 = QFont()
+        font1.setBold(True)
+        font1.setWeight(75)
+        self.label.setFont(font1)
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.spacingNum_spinBox = QSpinBox(Form)
+        self.spacingNum_spinBox.setObjectName(u"spacingNum_spinBox")
+        self.spacingNum_spinBox.setValue(3)
+
+        self.horizontalLayout.addWidget(self.spacingNum_spinBox)
+
+        self.LSJoint_pushButton = QPushButton(Form)
+        self.LSJoint_pushButton.setObjectName(u"LSJoint_pushButton")
+
+        self.horizontalLayout.addWidget(self.LSJoint_pushButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.line = QFrame(Form)
+        self.line.setObjectName(u"line")
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line)
+
+        self.label_5 = QLabel(Form)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setFont(font1)
+        self.label_5.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_5)
+
+        self.label_6 = QLabel(Form)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_6)
+
+        self.main_pushButton = QPushButton(Form)
+        self.main_pushButton.setObjectName(u"main_pushButton")
+
+        self.verticalLayout.addWidget(self.main_pushButton)
+
+        self.line_2 = QFrame(Form)
+        self.line_2.setObjectName(u"line_2")
+        self.line_2.setFrameShape(QFrame.HLine)
+        self.line_2.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_2)
+
+        self.label_8 = QLabel(Form)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setFont(font1)
+        self.label_8.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_8)
+
+        self.label_7 = QLabel(Form)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_7)
+
+        self.surfParamJoint_pushButton = QPushButton(Form)
+        self.surfParamJoint_pushButton.setObjectName(u"surfParamJoint_pushButton")
+
+        self.verticalLayout.addWidget(self.surfParamJoint_pushButton)
+
+        self.line_3 = QFrame(Form)
+        self.line_3.setObjectName(u"line_3")
+        self.line_3.setFrameShape(QFrame.HLine)
+        self.line_3.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_3)
+
+        self.label_9 = QLabel(Form)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setFont(font1)
+        self.label_9.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_9)
+
+        self.label_4 = QLabel(Form)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_4)
+
+        self.hybrid_pushButton = QPushButton(Form)
+        self.hybrid_pushButton.setObjectName(u"hybrid_pushButton")
+
+        self.verticalLayout.addWidget(self.hybrid_pushButton)
+
+        self.line_4 = QFrame(Form)
+        self.line_4.setObjectName(u"line_4")
+        self.line_4.setFrameShape(QFrame.HLine)
+        self.line_4.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_4)
+
+        self.label_10 = QLabel(Form)
+        self.label_10.setObjectName(u"label_10")
+        self.label_10.setFont(font1)
+        self.label_10.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_10)
+
+        self.label_11 = QLabel(Form)
+        self.label_11.setObjectName(u"label_11")
+        self.label_11.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_11)
+
+        self.attach_pushButton = QPushButton(Form)
+        self.attach_pushButton.setObjectName(u"attach_pushButton")
+
+        self.verticalLayout.addWidget(self.attach_pushButton)
+
+        self.line_6 = QFrame(Form)
+        self.line_6.setObjectName(u"line_6")
+        self.line_6.setFrameShape(QFrame.HLine)
+        self.line_6.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_6)
+
+        self.label_13 = QLabel(Form)
+        self.label_13.setObjectName(u"label_13")
+        self.label_13.setFont(font1)
+        self.label_13.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_13)
+
+        self.label_15 = QLabel(Form)
+        self.label_15.setObjectName(u"label_15")
+        self.label_15.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_15)
+
+        self.switchImport_pushButton = QPushButton(Form)
+        self.switchImport_pushButton.setObjectName(u"switchImport_pushButton")
+
+        self.verticalLayout.addWidget(self.switchImport_pushButton)
+
+        self.line_7 = QFrame(Form)
+        self.line_7.setObjectName(u"line_7")
+        self.line_7.setFrameShape(QFrame.HLine)
+        self.line_7.setFrameShadow(QFrame.Sunken)
+
+        self.verticalLayout.addWidget(self.line_7)
+
+        self.label_14 = QLabel(Form)
+        self.label_14.setObjectName(u"label_14")
+        self.label_14.setFont(font1)
+        self.label_14.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_14)
+
+        self.label_16 = QLabel(Form)
+        self.label_16.setObjectName(u"label_16")
+        self.label_16.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_16)
+
+        self.shapeChange_pushButton = QPushButton(Form)
+        self.shapeChange_pushButton.setObjectName(u"shapeChange_pushButton")
+
+        self.verticalLayout.addWidget(self.shapeChange_pushButton)
+
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.label_12.setText(QCoreApplication.translate("Form", u"Rig Support Tool", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Linear Spacing Joint", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"# Select the top joints and set (default [xAxis, zUp])", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Spacing Number : ", None))
+        self.LSJoint_pushButton.setText(QCoreApplication.translate("Form", u"Set", None))
+        self.label_5.setText(QCoreApplication.translate("Form", u"Main Setting", None))
+        self.label_6.setText(QCoreApplication.translate("Form", u"# Select the top joint and build", None))
+        self.main_pushButton.setText(QCoreApplication.translate("Form", u"Main FK / IK Build", None))
+        self.label_8.setText(QCoreApplication.translate("Form", u"Surface Parameter Get Position Joint", None))
+        self.label_7.setText(QCoreApplication.translate("Form", u"# Select the surface and get", None))
+        self.surfParamJoint_pushButton.setText(QCoreApplication.translate("Form", u"Get Joint", None))
+        self.label_9.setText(QCoreApplication.translate("Form", u"Hybrid Setting", None))
+        self.label_4.setText(QCoreApplication.translate("Form", u"# Select the top joints and build", None))
+        self.hybrid_pushButton.setText(QCoreApplication.translate("Form", u"Hybrid FK / IK Build", None))
+        self.label_10.setText(QCoreApplication.translate("Form", u"Attach Setting", None))
+        self.label_11.setText(QCoreApplication.translate("Form", u"# Select the curve and build", None))
+        self.attach_pushButton.setText(QCoreApplication.translate("Form", u"Attach Build", None))
+        self.label_13.setText(QCoreApplication.translate("Form", u"Switch Setting", None))
+        self.label_15.setText(QCoreApplication.translate("Form", u"#  Switch.ma file import", None))
+        self.switchImport_pushButton.setText(QCoreApplication.translate("Form", u"Import", None))
+        self.label_14.setText(QCoreApplication.translate("Form", u"Curve Shape Change", None))
+        self.label_16.setText(QCoreApplication.translate("Form", u"# Select[0] : Item / Select[1] : Target", None))
+        self.shapeChange_pushButton.setText(QCoreApplication.translate("Form", u"Change", None))
+    # retranslateUi
+
