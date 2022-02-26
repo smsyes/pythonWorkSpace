783c62a98327772213eb7693d3156943526315a3
sms <maruzou89@gmail.com>
2022-02-27 01:12:37 +0900

    20220227

---
+++ main/mApplication/ms_module/lib/etc/tantacle/tantacles.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""============================================================================
+기본 MiscTools를 사용하여 Tantacle 리깅 진행 후 Stretch setup등 추가.
+============================================================================"""
+import pymel.core as pm
+
+# item의 type 확인
+def isPC(item):
+    if item.type()=='pointOnCurveInfo':
+        return item
+
+# normalize값 연결해줄 multDobleLinear생성 및 연결.
+def parameterConnection(curve_):
+    list_ = curve_.getShape().ws.listConnections()
+    PCs = list(map(isPC, list_))
+
+    paramMLs = []
+    for i in PCs:
+        name_ = i.name()
+        slide = i.parameter.listConnections()
+        if slide:
+            i.parameter.disconnect()
+            paramML = pm.shadingNode('multDoubleLinear',au=1,n='{0}ParamML'.format(name_))
+            paramML.i1.set(1)
+            slide[0].ocr >> paramML.i2
+            paramML.o >> i.parameter
+            paramMLs.append(paramML)
+
+# 커브 길이 노말라이즈 값 연결
+def StretchConnection(curve_, paramMLs):
+    crvshape = curve_.getShape()
+    CI = pm.shadingNode('curveInfo',au=1,n='{0}CI'.format(sel[0].name()))
+    MD = pm.shadingNode('multiplyDivide',au=1,n='{0}NormalizeMD'.format(sel[0].name()))
+    BC = pm.shadingNode('blendColors',au=1,n='{0}StretchBC'.format(sel[0].name()))
+    ML = pm.shadingNode('multDoubleLinear',au=1,n='{0}StretchML'.format(sel[0].name()))
+    ML.i2.set(0.1)
+    crvshape.ws >> CI.inputCurve
+    CI.arcLength >> MD.i2x
+    length_ = CI.arcLength.get()
+    MD.i1x.set(length_)
+    MD.ox >> BC.c2r
+    ML.o >> BC.blender
+    list(map(lambda p: BC.outputR >> p.i1 , paramMLs))
+
+# 커브 선택후 실행.
+sel = pm.ls(sl=1,fl=1,r=1)
+paramMLs = parameterConnection(sel[0])
+StretchConnection(sel[0], paramMLs)
