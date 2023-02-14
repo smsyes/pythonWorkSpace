715845e71f1afe2c0725c853946d8d09cd5f2a86
smsyes <maruzou89@gmail.com>
2023-02-14 18:08:21 +0900

    20230214

--- main/mApplication/ms_module/maya/python3/rigSupport/lib/fingerSub.py
+++ main/mApplication/ms_module/maya/python3/rigSupport/lib/fingerSub.py
@@ -2,6 +2,16 @@
 # -*- coding: utf-8 -*-
 """============================================================================
 finger sub joint setup
+
+조인트 네개를 선택하고 실행합니다.
+1. 회전값을 주는 관절 finger조인트 Select List [0]
+2. 회전값의 절반만 받는 해당 관절 조인트 상위에 parent된 subRoot조인트 (fingerParent/subRoot) Select List [1]
+3. 회전값에 따라 driven값이 적용되는 sub조인트 (subRoot/sub) Select List [1:]
+4. fingerSubSet(Drive attr, Driven attr, [driveValue list], [setValue list], ivs_=True)
+5. ivs_ True인 경우 inverse 
+
+# fingerSubSet('rz', 'ty', [0,-90], [0.3,1], ivs_=True)
+
 ============================================================================"""
 import pymel.core as pm
 
@@ -29,11 +39,19 @@ def setDriven_(iAttr,tAttr,dv_,v_):
     for i,d in enumerate(dv_):
         pm.setDrivenKeyframe(tAttr,cd=iAttr,dv=d,v=v_[i])
 
-def fingerSDK(iAttr,tAttr,dv_,v_):
-    setDriven_(iAttr,tAttr,dv_,v_)
+def fingerSubSet(iAttr, tAttr, dvValue, dvnValue,ivs_=None):
+    if not  isinstance(dvValue, list):
+        dvValue = list(dvValue)
+    if not  isinstance(dvnValue, list):
+        dvnValue = list(dvnValue)
+    sel = pm.ls(sl=1,fl=1,r=1)
+    halfRot(sel[:2])
+    for i,s in enumerate(sel[2:]):
+        if not (i%2==1):
+            if ivs_ == True:
+                dvnReValue = [-1*v for v in dvnValue]
+        else:
+            dvnReValue = dvnValue
+        setDriven_(sel[0].attr(iAttr),s.attr(tAttr),dvValue,dvnReValue)
 
-sel = pm.ls(sl=1,fl=1,r=1)
-# halfRot(sel)
-# fingerSDK(sel[0].ry,sel[1].tz,[45],[-1])
-# fingerSDK(sel[0].ry,sel[2].tz,[45],[1])
-setDriven_(iAttr,tAttr,dv_,v_)
+fingerSubSet('rz', 'ty', [0,-90], [0.3,1], ivs_=True)
