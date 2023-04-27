5aa785353edd505c333c15a043e2357b69018612
smsyes <maruzou89@gmail.com>
2023-04-28 08:39:22 +0900

    20230428

---
+++ main/mApplication/ms_module/maya/python3/rigSupport/lib/_offsetParentMatrix.py
@@ -0,0 +1,56 @@
+import pymel.core as pm
+
+def halfList_(list_):
+    half = int(len(list_)/2)
+    return list_[:half], list_[half:]
+
+def driveOffset_(item):
+    dm_ = pm.shadingNode('multMatrix',au=1,n='{0}_DM'.format(item.name()))
+    dm_.matrixIn[0].set(item.wim.get())
+    item.wm >> dm_.matrixIn[1]
+    return dm_
+    
+def offsetMatrix_(list_):
+    item_, target_ = halfList_(list_)
+    for i,item in enumerate(item_):
+        dm_ = driveOffset_(item)
+        dm_.matrixSum >> target_[i].offsetParentMatrix
+
+def pickMatrix_(list_, type_=[]):
+    item_, target_ = halfList_(list_)
+    for i,item in enumerate(item_):
+        pic_ = pm.createNode('pickMatrix', n='{0}_PickM'.format(item.name()))
+        dm_ = driveOffset_(item)
+        dm_.matrixSum >> pic_.inputMatrix
+        pic_.outputMatrix >> target_[i].offsetParentMatrix
+        useMatrix_(pic_, type_)   
+        
+def blendMatrix_(list_, type_=[]):
+    item_, target_ = list_[:-1], list_[-1]
+    bm_ = pm.createNode('blendMatrix', n='{0}_BM'.format(target_.name()))
+    for i,item in enumerate(item_):
+        dm_ = driveOffset_(item)
+        dm_.matrixSum >> bm_.target[i].targetMatrix
+    bm_.outputMatrix >> target_.offsetParentMatrix
+    useMatrix_(bm_, type_)
+        
+def useMatrix_(node_, type_):
+    if not type_:
+            pass
+    else:
+        if not 't' in type_:
+            node_.useTranslate.set(0)
+        if not 'r' in type_:
+            node_.useRotate.set(0)
+        if not 's' in type_:
+            node_.useScale.set(0)
+        if not 'sh' in type_:
+            node_.useShear.set(0)
+
+        
+sel = pm.ls(sl=1,fl=1,r=1)
+# offsetMatrix_(sel)
+# pickMatrix_(sel)
+# blendMatrix_(sel)
+
+
--- main/mApplication/ms_module/maya/python3/rigSupport/lib/advTpose.py
+++ main/mApplication/ms_module/maya/python3/rigSupport/lib/advTpose.py
@@ -1,19 +1,55 @@
-import maya.cmds as cmds
-cmds.spaceLocator(n = 'FK_arm_R_loc', p = (0, 0, 0))
-cmds.setAttr('FK_arm_R_loc.rotate', 90, 0, -180, type="double3")
-cmds.spaceLocator(n = 'FK_arm_L_loc', p = (0, 0, 0))
-cmds.setAttr('FK_arm_L_loc.rotate', -90, 0, 180, type="double3")
-cmds.spaceLocator(n = 'FK_leg_R_loc', p = (0, 0, 0))
-cmds.setAttr('FK_leg_R_loc.rotate', 90, 0, -90, type="double3")
-cmds.spaceLocator(n = 'FK_leg_L_loc', p = (0, 0, 0))
-cmds.setAttr('FK_leg_L_loc.rotate', -90, 0, 90, type="double3")
-cmds.setAttr ('FKIKLeg_R.FKIKBlend', 0)
-cmds.setAttr ('FKIKArm_R.FKIKBlend', 0)
-cmds.setAttr ('FKIKArm_L.FKIKBlend', 0)
-cmds.setAttr ('FKIKSpine_M.FKIKBlend', 0)
-cmds.setAttr ('FKIKLeg_L.FKIKBlend', 0)
-cmds.matchTransform('FKScapula_R', 'FKShoulder_R', 'FKElbow_R', 'FKWrist_R', 'FK_arm_R_loc', rot = True)
-cmds.matchTransform('FKScapula_L', 'FKShoulder_L', 'FKElbow_L', 'FKWrist_L', 'FK_arm_L_loc', rot = True)
-cmds.matchTransform('FKHip_R', 'FKKnee_R', 'FKAnkle_R', 'FK_leg_R_loc', rot = True)
-cmds.matchTransform('FKHip_L', 'FKKnee_L', 'FKAnkle_L', 'FK_leg_L_loc', rot = True)
-cmds.delete('FK_arm_R_loc', 'FK_arm_L_loc', 'FK_leg_R_loc', 'FK_leg_L_loc')
+import pymel.core as pm
+import pymel.core.datatypes as dt
+
+sel = pm.ls(sl=1)[0]
+if sel.namespace():
+    ns_ = sel.namespace()
+else:
+    ns_ = ''
+
+arm_R_m = dt.Matrix()
+arm_R_m[0] = [-1.0, -1.2246467991473532e-16, 0.0, 0.0]
+arm_R_m[1] = [0.0, 0.0, 1.0, 0.0]
+arm_R_m[2] = [-1.2246467991473532e-16, 1.0, 0.0, 0.0]
+arm_R_m[3] = [0.0, 0.0, 0.0, 1.0]
+
+arm_L_m = dt.Matrix()
+arm_L_m[0] = [-1.0, 1.2246467991473532e-16, 0.0, 0.0]
+arm_L_m[1] = [0.0, 0.0, -1.0, 0.0]
+arm_L_m[2] = [-1.2246467991473532e-16, -1.0, 0.0, 0.0]
+arm_L_m[3] = [0.0, 0.0, 0.0, 1.0]
+
+leg_R_m = dt.Matrix()
+leg_R_m[0] = [0.0, -1.0, 0.0, 0.0]
+leg_R_m[1] = [0.0, 0.0, 1.0, 0.0]
+leg_R_m[2] = [-1.0, 0.0, 0.0, 0.0]
+leg_R_m[3] = [0.0, 0.0, 0.0, 1.0]
+
+leg_L_m = dt.Matrix()
+leg_L_m[0] = [0.0, 1.0, 0.0, 0.0]
+leg_L_m[1] = [0.0, 0.0, -1.0, 0.0]
+leg_L_m[2] = [-1.0, 0.0, 0.0, 0.0]
+leg_L_m[3] = [0.0, 0.0, 0.0, 1.0]
+
+m_list = [arm_R_m,arm_L_m,leg_R_m,leg_L_m]
+pos_list = []
+for i in m_list:
+    pos_ = pm.spaceLocator()
+    pos_.setMatrix(i)
+    pos_list.append(pos_)
+
+FKIKList = ['FKIKLeg_R','FKIKArm_R','FKIKArm_L','FKIKSpine_M','FKIKLeg_L']
+[pm.PyNode(ns_ + i).FKIKBlend.set(0) for i in FKIKList]
+
+match_dict = {
+    0 : ['FKScapula_R', 'FKShoulder_R', 'FKElbow_R', 'FKWrist_R'],
+    1 : ['FKScapula_L', 'FKShoulder_L', 'FKElbow_L', 'FKWrist_L'],
+    2 : ['FKHip_R', 'FKKnee_R', 'FKAnkle_R'],
+    3 : ['FKHip_L', 'FKKnee_L', 'FKAnkle_L']
+    }
+
+for keys in match_dict.keys():
+    for value in match_dict[keys]:
+        pm.matchTransform(pm.PyNode(ns_ + value), pos_list[keys], rot=True)
+    
+pm.delete(pos_list)
