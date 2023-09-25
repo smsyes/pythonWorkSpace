11f9fa2cf5fa758ddd34136a7681b653f919f8ca
smsyes <maruzou89@gmail.com>
2023-09-25 14:02:21 +0900

    20230925

---
+++ main/mApplication/ms_module/maya/python3/rigSupport/test/test.py
@@ -0,0 +1,10 @@
+language = int(input("언어를 입력하세요(1=한국어, 2=영어, 3=프랑스어, 4=독일어)"))
+
+if language ==1:
+    print("안녕")
+elif language ==2:
+    print("Hello")
+elif language ==3:
+    print("Bonjour")
+elif language ==4:
+    print("Guten morgen")
