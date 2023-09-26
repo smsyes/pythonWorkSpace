ae4f531c1b0f44ec754e43b17b3fffc190c9ce1b
smsyes <maruzou89@gmail.com>
2023-09-26 15:43:24 +0900

    20230926

--- main/mApplication/ms_module/maya/python3/rigSupport/lib/constToMCon.py
+++ main/mApplication/ms_module/maya/python3/rigSupport/lib/constToMCon.py
@@ -13,10 +13,7 @@ def createQM_(object_,mo_=1):
         name_ = item.name()
         MM = pm.createNode('multMatrix', n='{}_MM'.format(name_))
         DM = pm.createNode('decomposeMatrix', n='{}_DM'.format(name_))
-        EQ = pm.shadingNode('eulerToQuat', au=1, n='{}_EQ'.format(name_))
-        QI = pm.shadingNode('quatInvert', au=1, n='{}_QI'.format(name_))
-        QP = pm.shadingNode('quatProd', au=1, n='{}_QP'.format(name_))
-        QE = pm.shadingNode('quatToEuler', au=1, n='{}_QE'.format(name_))
+
         if mo_ == 0:
             MM.matrixIn[1].set(target_[i].getMatrix(worldSpace=True))
             MM.matrixIn[2].set(item.getMatrix(worldSpace=True).inverse())
@@ -26,26 +23,24 @@ def createQM_(object_,mo_=1):
             MM.matrixIn[0].set(tgScalePivotMtx)
             MM.matrixIn[1].set(target_[i].getMatrix(worldSpace=True))
             MM.matrixIn[2].set(item.getMatrix(worldSpace=True).inverse())
+        target_[i].r.set(target_[i].jo.get())
+        target_[i].jo.set(0,0,0)
         item.wm >> MM.matrixIn[3]
         target_[i].pim >> MM.matrixIn[4]
         MM.matrixSum >> DM.inputMatrix
-        if target_[i].type() == 'joint':
-            target_[i].jo >> EQ.inputRotate
-        else:
-            target_[i].r >> EQ.inputRotate
-        EQ.outputQuat >> QI.inputQuat
-        DM.outputQuat >> QP.input1Quat
-        QI.outputQuat >> QP.input2Quat
-        QP.outputQuat >> QE.inputQuat
         DM.ot >> target_[i].t
-        target_[i].r.set(0,0,0)
-        QE.outputRotate >> target_[i].jo
+        DM.outputRotate >> target_[i].r
+        DM.os >> target_[i].s
+        
 
-sel = pm.ls(sl=1,r=1,fl=1)
-for i in sel:
+sel = pm.ls(sl=1,r=1)
+dir(sel[0].__class__)
+list_ = sel[0].listRelatives(ad=1,typ='parentConstraint')
+
+for i in list_:
     item_ = i.target.listConnections(s=1,d=0)[0]
     target_ = i.constraintParentInverseMatrix.listConnections(s=1,d=0)[0]
+    if target_.sx.listConnections(s=1,d=0,type='scaleConstraint') :
+        pm.delete(target_.sx.listConnections(s=1,d=0,type='scaleConstraint'))
     pm.delete(i)
     createQM_([item_, target_],mo_=1)
-    
-
